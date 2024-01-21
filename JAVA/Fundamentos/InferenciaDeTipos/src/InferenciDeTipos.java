2834eddff0f4b2be3da7037904d6b96b311bf3e8
Juan Alonso <jjriosalonso@gmail.com>
2024-01-21 15:43:42 -0600

    Variable con inferencia de tipos, se requiere JDK 10 o superior

---
+++ JAVA/Fundamentos/InferenciaDeTipos/src/InferenciDeTipos.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author juan.rios
+ */
+public class InferenciDeTipos {
+    public static void main(String[] args) {
+        
+        //Para la inferencia de tipos podemos usar la palabra reservada var
+        var miVariableEntera = 10;
+        System.out.println("var = " + var);
+    }
+}
