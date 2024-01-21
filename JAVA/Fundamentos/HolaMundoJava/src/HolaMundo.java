89c49a9ef897868a73e2d95e197cb8420079c6c0
Juan Alonso <jjriosalonso@gmail.com>
2024-01-21 14:29:02 -0600

    Carpeta JAVA

---
+++ JAVA/Fundamentos/HolaMundoJava/src/HolaMundo.java
@@ -0,0 +1,13 @@
+/**
+ *
+ * @author juan.rios
+ */
+public class HolaMundo {
+    
+    //Metodo o funcion principal (main), este metodo es el que nos permite iniciar o ejecutar el programa
+    public static void main(String[] args) {
+        
+        //Este parte del codigo nos permite mandar por consola la cadena Hola Mundo
+        System.out.println("Hola Mundo");
+    }
+}
